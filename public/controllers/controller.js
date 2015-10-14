5fdce9ddbe70479db9f9a0665438b0d8e14d63ab
sol <solguttman@gmail.com>
2015-10-14 13:41:22 -0400

    added the option to delete and update

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,7 +1,7 @@
 var app = angular.module('app',[]);
 
 app.controller('AppCtrl',['$scope','$http',function($scope,$http){
-	console.log('Hello from controller');
+	//console.log('Hello from controller');
 	
 	var refresh = function(){
 		$http.get('/contactlist').success(function(res){
@@ -19,6 +19,31 @@ app.controller('AppCtrl',['$scope','$http',function($scope,$http){
 			//console.log(res);
 			refresh();
 		});
+	};
+	
+	$scope.remove = function(id){
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(res){
+			refresh();
+		});
+	};
+	
+	$scope.edit = function(id){
+		console.log(id);
+		$http.get('/contactlist/' + id).success(function(res){
+			$scope.contact = res;
+		});
+	};
+	
+	$scope.updateContact = function(){
+		console.log($scope.contact._id);
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(res){
+			refresh();
+		});
+	};
+	
+	$scope.deselect = function(){
+		$scope.contact = "";
 	};
 	
 }]);
