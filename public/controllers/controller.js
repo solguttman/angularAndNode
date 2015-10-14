312887fe811b6a65d5d1622fddeff821d43657ea
sol <solguttman@gmail.com>
2015-10-14 12:14:23 -0400

    made connection from front end to the back end

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,11 +1,10 @@
 var app = angular.module('app',[]);
 
-
-
-/* var AppCtrl = function($scope,$http){
-	console.log('Hello from controller');
-}; */
-
 app.controller('AppCtrl',['$scope','$http',function($scope,$http){
 	console.log('Hello from controller');
+	
+	$http.get('/contactlist').success(function(res){
+		console.log(res);
+		$scope.contactlist = res;
+	});
 }]);
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 var express = require('express');
+//var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
+app.get('/contactlist',function(req,res){
+	console.log('recved get reques');
+	
+	person1 = {
+		name : 'tim',
+		email : 'tim@gmail.com',
+		number : '111-111-1111' 
+	};
+	person2 = {
+		name : 'sol',
+		email : 'sol@gmail.com',
+		number : '222-222-2222' 
+	};
+	person3 = {
+		name : 'john',
+		email : 'john@gmail.com',
+		number : '333-333-3333' 
+	};
+	
+	var contactlist = [person1,person2,person3];
+	res.json(contactlist);
+});
 
 app.listen(3000);
 
