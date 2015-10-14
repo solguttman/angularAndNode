90da0a86a7fe6ca72a7378b26f19aeffcd18f8af
sol <solguttman@gmail.com>
2015-10-14 13:03:48 -0400

    connected to db and start to get data and insert data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,8 +3,22 @@ var app = angular.module('app',[]);
 app.controller('AppCtrl',['$scope','$http',function($scope,$http){
 	console.log('Hello from controller');
 	
-	$http.get('/contactlist').success(function(res){
-		console.log(res);
-		$scope.contactlist = res;
-	});
+	var refresh = function(){
+		$http.get('/contactlist').success(function(res){
+			//console.log(res);
+			$scope.contactlist = res;
+			$scope.contact = "";
+		});
+	};
+	
+	refresh();
+	
+	$scope.addContact = function(){
+		//console.log($scope.contact);
+		$http.post('/contactlist',$scope.contact).success(function(res){
+			//console.log(res);
+			refresh();
+		});
+	};
+	
 }]);
--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 var express = require('express');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist']);
+
 app.use(express.static(__dirname + '/public'));
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get('/contactlist',function(req,res){
 	console.log('recved get reques');
 	
-	person1 = {
-		name : 'tim',
-		email : 'tim@gmail.com',
-		number : '111-111-1111' 
-	};
-	person2 = {
-		name : 'sol',
-		email : 'sol@gmail.com',
-		number : '222-222-2222' 
-	};
-	person3 = {
-		name : 'john',
-		email : 'john@gmail.com',
-		number : '333-333-3333' 
-	};
+	db.contactlist.find(function(err,docs){
+		//console.log(docs);
+		res.json(docs);
+	});
 	
-	var contactlist = [person1,person2,person3];
-	res.json(contactlist);
+});
+
+app.post('/contactlist',function(req, res){
+	//console.log(req.body);
+	db.contactlist.insert(req.body,function(err,doc){
+		res.json(doc);
+	});
 });
 
 app.listen(3000);
