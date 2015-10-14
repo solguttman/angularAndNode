fe7dd2b6c358bdedf0cd82cdfe2ceae1c7765964
sol <solguttman@gmail.com>
2015-10-14 11:27:03 -0400

    laid foundation for the app

---
+++ public/controllers/controller.js
@@ -0,0 +1,11 @@
+var app = angular.module('app',[]);
+
+
+
+/* var AppCtrl = function($scope,$http){
+	console.log('Hello from controller');
+}; */
+
+app.controller('AppCtrl',['$scope','$http',function($scope,$http){
+	console.log('Hello from controller');
+}]);
